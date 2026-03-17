70adc0e77dc6b2a21eb68c025f0e0dad6555ee61
Dani <danipro1406@gmail.com>
2026-03-17 11:54:08 -0500

    fix: typo pantalla_misiones en router.py

--- router.py
+++ router.py
@@ -6,7 +6,7 @@ from screen_login import pantalla_login
 from screen_juego import pantalla_juego
 from screen_fin import pantalla_fin
 from screen_ranking import pantalla_ranking
-from screen_misiones import pantalla_misione
+from screen_misiones import pantalla_misiones
 
 
 def router():
